4cf4407f06d1049dd5c3bbf44cd09b12c8a120ff
Eric Plaster <plaster@gmail.com>
2012-03-11 18:29:31 -0500

    pep8 cleanup

--- wsgi.py
+++ wsgi.py
@@ -9,14 +9,14 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Fix for dev mode
 if base_dir == "/project":
-  base_dir = "/server/env.example.com/project/"
+    base_dir = "/server/env.example.com/project/"
 
 # Get the site dir
-site_dir = os.path.join(base_dir,"../lib/python2.6/site-packages")
+site_dir = os.path.join(base_dir, "../lib/python2.6/site-packages")
 site.addsitedir(site_dir)
 
 # Adjust the system path
-sys.path.append(os.path.join(base_dir,"project/"))
+sys.path.append(os.path.join(base_dir, "project/"))
 sys.path.append(os.path.join(base_dir, "project/core/"))
 
 # Setup the settings module
@@ -26,4 +26,3 @@ os.environ["CELERY_LOADER"] = "django"
 # Load django
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
-
